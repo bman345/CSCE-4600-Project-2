30e014db2e53d0d6fba7a01982964dd99e299c98
CamronVZ <camronveazey@gmail.com>
2023-05-11 01:15:36 -0500

    Updated Comments For Repeat

--- Project2/builtins/repeat.go
+++ Project2/builtins/repeat.go
@@ -6,10 +6,9 @@ import (
 	"os"
 	"os/exec"
 	"io"
-	
-	//"github.com/jh125486/CSCE4600/Project2/builtins"
 )
 
+// Function that checks if the given command to repeat is a valid command.
 func Check(w io.Writer, nameR string, argsR ...string) error {
     switch nameR {
     case "cd":
@@ -24,19 +23,22 @@ func Check(w io.Writer, nameR string, argsR ...string) error {
     return executeCommand(nameR, argsR...)
 }
 
+// The main function for the repeat command.
 func Repeat(w io.Writer, args ...string) error {
     num, err := strconv.Atoi(args[0])
     if err != nil {
         return fmt.Errorf("%w: Input for loop number must be a positive number.", ErrInvalidArgCount)
     }
-
+	
+	// New name and args that are specifically for the looping command.
     nameR, argsR := args[1], args[2:]
-
+	
+	// Make sure that the given input is valid and if it is, repeat the given command "num" times.
     if num <= 0 {
         return fmt.Errorf("%w: Input for loop number cannot be zero or less than zero.", ErrInvalidArgCount)
     } else if num > 0 {
         for num > 0 {
-            err := Check(w, nameR, argsR...) // assign the error value returned by Check to a variable
+            err := Check(w, nameR, argsR...) // Assign the error value returned by Check to a variable.
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%v\n", err)
 			}
@@ -48,6 +50,7 @@ func Repeat(w io.Writer, args ...string) error {
     return nil
 }
 
+// Funciton specifically for running the command that is being repeated.
 func executeCommand(nameR string, argsR ...string) error {
     cmd := exec.Command(nameR, argsR...)
     cmd.Stderr = os.Stderr
